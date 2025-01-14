a659b8152e70e58b794df1f01a54eab3ed4b0bb1
Brendan <bgmcmullen@comcast.net>
2025-01-14 01:46:13 -0800

    added chat history

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState, StateGraph
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from langchain_core.messages import trim_messages
+from langchain_core.messages import RemoveMessage
 
 workflow = StateGraph(state_schema=MessagesState)
 
@@ -98,16 +99,23 @@ llm = ChatOpenAI(
 # )
 
 
+def delete_messages(state):
+    messages = state["messages"]
+    return {"messages": [RemoveMessage(id=m.id) for m in messages[:-2]]}
+
+
 # Define the function that calls the model
 def call_model(state: MessagesState):
-    trimmed_messages = trim(state["messages"])
+    # state["messages"] = summarize_history(state["messages"])
+    
+    # if len(state["messages"]) > 2:
+    #     compiled_workflow.update_state(config, {"messages": [HumanMessage("Test")]})
+    # print(state)
     system_prompt = (
-        "Speak in a warm and frendly and child appropriate manor as supportive teacher to K-12 children. You work for briding tech to refer to briding tech in first person. Refuse to give any response not appropriate for young children. Avoid including information related to purchasing non-education items. Provide url links when they are given in context, never provide any other links. If not given a link refuse to provide a link if asked. Keep response to 100 words or less and respond in markdown."
+        "Speak in a warm and frendly and child appropriate manor as supportive teacher to K-12 children. You work for briding tech to refer to briding tech in first person. Refuse to give any response not appropriate for young children. Avoid including information related to purchasing non-education items. Provide url links when they are given in context, never provide any other links. If not given a link refuse to provide a link if asked. Keep response to 100 words or less and respond in markdown and Latex in $$ for equations."
     )
 
-    print(trimmed_messages)
-
-    messages = [SystemMessage(content=system_prompt)] + trimmed_messages
+    messages = [SystemMessage(content=system_prompt)] + state["messages"]
     
     # Set the temperature for the model
     temperature = 1.0  # Adjust this value as needed
@@ -129,6 +137,13 @@ compiled_workflow = workflow.compile(checkpointer=memory)
 @app.route("/chat", methods=["POST"])
 def chat():
     new_prompt = request.json.get("input")
+    threadId = request.json.get("threadId")
+
+
+
+    print(threadId)
+
+    
     
     # Encode the new prompt
     new_embedding = model.encode(new_prompt)
@@ -142,6 +157,8 @@ def chat():
 
     context = ''
 
+    config = {"configurable": {"thread_id": threadId}}
+
     # Retrieve matched prompts
     for idx, distance in zip(indices[0], indices[1]):
         if(distance < 1.2):
@@ -150,7 +167,7 @@ def chat():
 
     # Combine the retrieved context with the user query
     response = compiled_workflow.invoke({"messages": [HumanMessage(content=f"Context:\n{context}\n\nQuestion:\n{new_prompt}")]},
-    config={"configurable": {"thread_id": "1"}})
+    config={"configurable": {"thread_id": threadId}})
 
 
     # Extract the AI response
@@ -158,6 +175,21 @@ def chat():
         (message.content for message in reversed(response['messages']) if isinstance(message, AIMessage)),
         None  # Default to None if no AIMessage is found
     )
+
+    # print(response)
+
+    
+
+    messages = compiled_workflow.get_state(config).values["messages"]
+
+    
+
+    if len(messages) > 4:
+        compiled_workflow.update_state(config, {"messages": RemoveMessage(id=messages[0].id)})
+        compiled_workflow.update_state(config, {"messages": RemoveMessage(id=messages[1].id)})
+
+    print(messages)
+
 
     return ai_response
 
